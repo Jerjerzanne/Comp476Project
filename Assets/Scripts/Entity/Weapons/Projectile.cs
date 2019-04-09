9f079ec8f1a115f999667e73235e86ee28bb7cd9
Vong Thang <33109773+VongT@users.noreply.github.com>
2019-04-08 22:57:37 -0400

    Fixed bullets

--- Assets/Scripts/Entity/Weapons/Projectile.cs
+++ Assets/Scripts/Entity/Weapons/Projectile.cs
@@ -6,8 +6,9 @@ public class Projectile : MonoBehaviour
 {
     #region Constants
 
-    private const int wallLayer = 13;
     private const int playerLayer = 9;
+    private const int soldierLayer = 10;
+    private const int wallLayer = 13;
     private const int alienLayer = 16;
     #endregion
 
@@ -42,15 +43,25 @@ public class Projectile : MonoBehaviour
             Debug.Log("Projectile hit a wall");
             Destroy(this.gameObject);
         }
-        else if(other.gameObject.layer == playerLayer || other.gameObject.layer == alienLayer)
+        else if ((other.gameObject.layer == playerLayer || other.gameObject.layer == soldierLayer) && gameObject.name == "AlienBullet(Clone)")
         {
             other.gameObject.GetComponent<Destructible>().TakeDamage(this._damage);
             Destroy(this.gameObject);
             //Debug.Log("Projectile hit " + other.gameObject.name);
         }
+        else if ((other.gameObject.layer == playerLayer || other.gameObject.layer == alienLayer) && gameObject.name == "Bullet(Clone)")
+        {
+            other.gameObject.GetComponent<Destructible>().TakeDamage(this._damage);
+            Destroy(this.gameObject);
+            //Debug.Log("Projectile hit " + other.gameObject.name);
+        }
+        else if ((other.gameObject.layer == soldierLayer || other.gameObject.layer == alienLayer) && gameObject.name == "PlayerBullet(Clone)")
+        {
+            other.gameObject.GetComponent<Destructible>().TakeDamage(this._damage);
+            Destroy(this.gameObject);
+        }
 
-       
     }
-    
+
     #endregion
 }
