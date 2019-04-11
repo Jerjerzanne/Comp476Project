97d5d8fc3eabc4c9bba378a593a5026e0aedcb77
Vong Thang <33109773+VongT@users.noreply.github.com>
2019-04-11 02:58:17 -0400

    meat despawn

---
+++ Assets/Scripts/Entity/Meat.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meat : MonoBehaviour
+{
+    private IEnumerator Delete()
+    {
+        yield return new WaitForSeconds(20.0f);
+        Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        StartCoroutine("Delete");
+    }
+}
